63c1699ba0a5bed35399c289a35ad63b86599b8f
Ekaterina Nichiporenko <75068703+KatyaNichi@users.noreply.github.com>
2022-01-28 11:13:00 +0100

    added searchBar

---
+++ src/CardList.js
@@ -0,0 +1,24 @@
+import { render } from "@testing-library/react";
+import Card from "./Card";
+
+
+const CardList = ({ robots}) => {
+        return (
+            <div>
+            {
+                robots.map((user, index) => {
+                    return(
+                        <Card 
+                        key={index} 
+                        id={robots[index].id} 
+                        name={robots[index].name} 
+                        email={robots[index].email} 
+                        />
+                    );
+                })
+            }
+        </div>
+    );
+}
+
+export default CardList;
